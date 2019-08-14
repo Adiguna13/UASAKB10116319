a6f5990bf9beab268779b9eea81d3f5cc594aca5
Adiguna <adigunarizqi@gmail.com>
2019-08-14 14:54:58 +0700

    14

---
+++ app/src/main/java/adiguna/com/SplashScreenAct.java
@@ -0,0 +1,40 @@
+package adiguna.com;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+
+import adiguna.com.LoginActivity;
+import adiguna.com.R;
+
+/*
+    Tanggal Pengerjaan  : Sabtu 18 Mei 2019
+    NIM                 : 10116319
+    Nama                : Rizqi Adiguna
+    Kelas               : IF-7
+ */
+
+public class SplashScreenAct extends AppCompatActivity {
+    TextView Title;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        Title = (TextView)findViewById(R.id.Title);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        }, 3000L);
+    }
+}
